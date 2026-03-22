56d5a0645eb8bc18f96ebb66c37df4b16952d024
Random121 <59186475+Random121@users.noreply.github.com>
2026-03-22 15:06:27 -0700

    firmware/controls/pwm: add pwm module source

--- embedded-software/firmware/libs/controls/include/controls/pwm.h
+++ embedded-software/firmware/libs/controls/include/controls/pwm.h
@@ -3,9 +3,12 @@
 
 #include <stdint.h>
 
+#define PWM_MIN_PERIOD_US (1000)
+#define PWM_MAX_PERIOD_US (2000)
+
 typedef struct {
-  uint16_t upper_motor_us; /**< PWM for the upper motor in microseconds */
-  uint16_t lower_motor_us;
+    uint16_t upper_motor_us; /**< PWM for the upper motor in microseconds */
+    uint16_t lower_motor_us;
 } pwm_setpoint_t;
 
 pwm_setpoint_t pwm_setpoint_from_forces(double thrust, double torque);
