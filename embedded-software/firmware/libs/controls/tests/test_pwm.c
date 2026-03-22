b55e7d264c021f9c1d3e563b485b7436d1de344c
Random121 <59186475+Random121@users.noreply.github.com>
2026-03-22 15:14:57 -0700

    firmware/controls/tests: fix pwm tests

--- embedded-software/firmware/libs/controls/tests/test_pwm.c
+++ embedded-software/firmware/libs/controls/tests/test_pwm.c
@@ -1,40 +1,42 @@
 #include "unity.h"
+
 #include "controls/pwm.h"
+
 #include <math.h>
 
-#define TOL 20 /**< Tolerance for floating-point comparisons */
+#define TOL (20) /**< Tolerance for PWM period comparisons */
 
 void test_pwm_zeroes(void)
 {
-    pwm_setpoint_t setpoint_pwm = pwm_from_setpoint(0,0);
-    TEST_ASSERT_EQUAL_UINT16(1000, setpoint_pwm.upper_motor_us, TOL);
-    TEST_ASSERT_EQUAL_UINT16(1000, setpoint_pwm.lower_motor_us, TOL);
+    pwm_setpoint_t setpoint = pwm_setpoint_from_forces(0, 0);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1000, setpoint.lower_motor_us);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1000, setpoint.upper_motor_us);
 }
 
 void test_pwm_midpoint(void)
 {
-    pwm_setpoint_t setpoint_pwm = pwm_from_setpoint(500,0);
-    TEST_ASSERT_EQUAL_UINT16(1531.9, setpoint_pwm.upper_motor_us, TOL);
-    TEST_ASSERT_EQUAL_UINT16(1559.4, setpoint_pwm.lower_motor_us, TOL);
+    pwm_setpoint_t setpoint = pwm_setpoint_from_forces(500, 0);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1532, setpoint.lower_motor_us);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1559, setpoint.upper_motor_us);
 }
 
 void test_pwm_max_thrust(void)
 {
-    pwm_setpoint_t setpoint_pwm = pwm_from_setpoint(1594.56,0);
-    TEST_ASSERT_EQUAL_UINT16(1949.9, setpoint_pwm.upper_motor_us, TOL);
-    TEST_ASSERT_EQUAL_UINT16(1999.0, setpoint_pwm.lower_motor_us, TOL);
+    pwm_setpoint_t setpoint = pwm_setpoint_from_forces(1594.56, 0);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1950, setpoint.lower_motor_us);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1999, setpoint.upper_motor_us);
 }
 
 void test_pwm_max_torque_upper(void)
 {
-    pwm_setpoint_t setpoint_pwm = pwm_from_setpoint(782.16, 0.1952);
-    TEST_ASSERT_EQUAL_UINT16(1999.0, setpoint_pwm.upper_motor_us, TOL);
-    TEST_ASSERT_EQUAL_UINT16(1000.0, setpoint_pwm.lower_motor_us, TOL);
+    pwm_setpoint_t setpoint = pwm_setpoint_from_forces(782.16, 0.1952);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1999, setpoint.lower_motor_us);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1000, setpoint.upper_motor_us);
 }
 
 void test_pwm_max_torque_lower(void)
 {
-    pwm_setpoint_t setpoint_pwm = pwm_from_setpoint(887.44, -0.1765);
-    TEST_ASSERT_EQUAL_UINT16(1000.0, setpoint_pwm.upper_motor_us, TOL);
-    TEST_ASSERT_EQUAL_UINT16(1999.0, setpoint_pwm.lower_motor_us, TOL);
+    pwm_setpoint_t setpoint = pwm_setpoint_from_forces(887.44, -0.1765);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1000, setpoint.lower_motor_us);
+    TEST_ASSERT_UINT16_WITHIN(TOL, 1999, setpoint.upper_motor_us);
 }
