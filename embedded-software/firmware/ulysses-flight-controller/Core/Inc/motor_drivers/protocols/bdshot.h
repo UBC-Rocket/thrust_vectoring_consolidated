54d2024c0f98c809f2400442df56f8fa191ecccb
Random121 <59186475+Random121@users.noreply.github.com>
2026-04-09 22:02:11 -0700

    refactor: replace dshot with bdshot

---
+++ embedded-software/firmware/ulysses-flight-controller/Core/Inc/motor_drivers/protocols/bdshot.h
@@ -0,0 +1,28 @@
+#ifndef ULYSSES_MOTOR_DRIVER_PROTOCOLS_BDSHOT_H
+#define ULYSSES_MOTOR_DRIVER_PROTOCOLS_BDSHOT_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define BDSHOT_MIN_THROTTLE (48)
+#define BDSHOT_MAX_THROTTLE (2047)
+
+#define BDSHOT_THROTTLE_MASK  (0xFFE0)
+#define BDSHOT_THROTTLE_BITS  (11)
+#define BDSHOT_THROTTLE_SHIFT (5)
+
+#define BDSHOT_TELEMETRY_MASK  (0x0010)
+#define BDSHOT_TELEMETRY_BITS  (1)
+#define BDSHOT_TELEMETRY_SHIFT (4)
+
+#define BDSHOT_CHECKSUM_MASK  (0x000F)
+#define BDSHOT_CHECKSUM_BITS  (4)
+#define BDSHOT_CHECKSUM_SHIFT (0)
+
+typedef uint16_t bdshot_frame_t;
+
+bool bdshot_frame_pack(bdshot_frame_t *frame, uint16_t throttle, bool request_telemetry);
+
+uint8_t bdshot_frame_checksum(bdshot_frame_t frame);
+
+#endif // ULYSSES_MOTOR_DRIVER_PROTOCOLS_BDSHOT_H
