b9076936ca65c9006fb8b0a7104ce587a281bb2e
Steven Chen <109647201+Shengw3n@users.noreply.github.com>
2026-07-12 17:15:02 -0700

    chore: update new gimbal offset

--- embedded-software/firmware/dev/servo_dynamixel/include/dev_servo_dynamixel.h
+++ embedded-software/firmware/dev/servo_dynamixel/include/dev_servo_dynamixel.h
@@ -16,10 +16,10 @@ extern "C" {
 
 /* Calibrated mechanical zero: encoder ticks when gimbal is at 0°, 0°. */
 #ifndef DXL_ZERO_TICKS_X
-#define DXL_ZERO_TICKS_X  190
+#define DXL_ZERO_TICKS_X  199
 #endif
 #ifndef DXL_ZERO_TICKS_Y
-#define DXL_ZERO_TICKS_Y  (-14)
+#define DXL_ZERO_TICKS_Y  1795
 #endif
 
 typedef struct servo_dynamixel servo_dynamixel_t;
