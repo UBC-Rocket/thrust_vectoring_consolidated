07ace29cec755413a0ff5a5a39e2649ee577b1f9
13carpileup <climie.alex@gmail.com>
2026-07-19 16:29:37 -0700

    calibration

--- embedded-software/firmware/dev/servo_dynamixel/include/dev_servo_dynamixel.h
+++ embedded-software/firmware/dev/servo_dynamixel/include/dev_servo_dynamixel.h
@@ -16,10 +16,10 @@ extern "C" {
 
 /* Calibrated mechanical zero: encoder ticks when gimbal is at 0°, 0°. */
 #ifndef DXL_ZERO_TICKS_X
-#define DXL_ZERO_TICKS_X  2444
+#define DXL_ZERO_TICKS_X  1932
 #endif
 #ifndef DXL_ZERO_TICKS_Y
-#define DXL_ZERO_TICKS_Y  1432
+#define DXL_ZERO_TICKS_Y  1756
 #endif
 
 typedef struct servo_dynamixel servo_dynamixel_t;
